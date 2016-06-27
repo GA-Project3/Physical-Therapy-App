347d315d64f86fb8e85b665e8d448bd2ab18ee4e
byfyang <darkspherico@gmail.com>
2016-06-27 11:47:57 -0700

    added home controller, home and about view

--- pt_app/config/routes.rb
+++ pt_app/config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+	##home page##
+	get '/', to: 'home#home', as: 'home'
+
+	##about page##
+	get '/about', to: 'home#about', as: 'about'
+
 	resources :doctors, :patients, :exercises
 
 end
