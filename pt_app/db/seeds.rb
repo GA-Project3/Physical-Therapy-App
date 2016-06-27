0d06f058c361a04c7ddc7b6cf8f0c582c7f268eb
Michael Norelli <mnorelli360@gmail.com>
2016-06-27 15:36:00 -0700

    adds patient-docotr relationships, and adds a migration to set up the foreign key in patients to doctor ID

--- pt_app/db/seeds.rb
+++ pt_app/db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Patient_Exercise.destroy_all
+PatientExercise.destroy_all
 Patient.destroy_all
 Exercise.destroy_all
 Doctor.destroy_all
@@ -24,18 +24,22 @@ doctors = Doctor.create([
     ])
 
 patients = Patient.create([
-  {first_name:"Kate",last_name:"Lincoln",email:"kl@gmail.com",location:"Beaverton, CO",phone:"303-555-4444",password:"password",
+  {first_name:"Kate",last_name:"Lincoln",email:"kl@gmail.com",location:"Beaverton, CO",phone_number:"303-555-4444",password:"password",
     diagnosis:"Torn L3 ligament, misaligned clavicle",
     image_url:"https://ucarecdn.com/a1272143-9cdf-49c3-9b9a-9e953b74d1e7/DefaultUserImage.png"},
-  {first_name:"Vinjay",last_name:"Sulman",email:"vinjay@yahoo.com",location:"Alexandria, VA",phone:"703-555-5555",password:"password",
+  {first_name:"Vinjay",last_name:"Sulman",email:"vinjay@yahoo.com",location:"Alexandria, VA",phone_number:"703-555-5555",password:"password",
     diagnosis:"Aggravated lower lumbar, hip malaplaysia",
     image_url:"https://ucarecdn.com/a1272143-9cdf-49c3-9b9a-9e953b74d1e7/DefaultUserImage.png"},
-  {first_name:"Leticia",last_name:"Abercrombie",email:"ladyw@hotmal.com",location:"Colorado Springs, CO",phone:"303-555-6666",password:"password",
+  {first_name:"Leticia",last_name:"Abercrombie",email:"ladyw@hotmal.com",location:"Colorado Springs, CO",phone_number:"303-555-6666",password:"password",
     diagnosis:"Sciatic crease, irregular spagiotic hemotoma",
     image_url:"https://ucarecdn.com/a1272143-9cdf-49c3-9b9a-9e953b74d1e7/DefaultUserImage.png"},
-  {first_name:"Dan",last_name:"Davis",email:"ddavis@msn.com",location:"Pittsburg, PA",phone:"412-555-7777",password:"password",
+  {first_name:"Dan",last_name:"Davis",email:"ddavis@msn.com",location:"Pittsburg, PA",phone_number:"412-555-7777",password:"password",
     diagnosis:"Torn humeral mass, shattered right tibia",
-    image_url:"https://ucarecdn.com/a1272143-9cdf-49c3-9b9a-9e953b74d1e7/DefaultUserImage.png"},
-
+    image_url:"https://ucarecdn.com/a1272143-9cdf-49c3-9b9a-9e953b74d1e7/DefaultUserImage.png"}
   ])
+
+patients[0].doctor_id = 0
+patients[1].doctor_id = 1
+patients[2].doctor_id = 0
+patients[3].doctor_id = 2
 
