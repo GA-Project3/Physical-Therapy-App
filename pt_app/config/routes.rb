7d0a4d29155cc54ae49d3a1d1b1935c09903c2ed
byfyang <darkspherico@gmail.com>
2016-06-27 17:07:14 -0700

    .

--- pt_app/config/routes.rb
+++ pt_app/config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-	##resources/rails created routes##
-	resources :doctors, :patients, :exercises
 
 	##home page##
 	get '/', to: 'home#home', as: 'home'
@@ -24,5 +22,7 @@ Rails.application.routes.draw do
 
 	get "exercises/:id", to: "exercises#show"
 
+	##resources/rails created routes##
+	resources :doctors, :patients, :exercises
 
 end
